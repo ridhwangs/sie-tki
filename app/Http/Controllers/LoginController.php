963282cc2a8f8cdd17b043a18f5f49ee0376579e
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2022-10-25 07:12:10 +0700

    add last route for login security

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,7 +21,8 @@ class LoginController extends Controller
  
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('dashboard');
+            $user = Auth::user();
+            return redirect()->route($user->last_route);
         }
         return back()->with('loginError', 'Login failed');
     }
